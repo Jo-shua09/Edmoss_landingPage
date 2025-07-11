06dfa4e810b1307b5a44653bfc2294ec398035a1
Jo-shua09 <joshuaonyeka039@gmail.com>
2025-07-11 02:04:27 -0700

    fixing errors

--- vite.config.js
+++ vite.config.js
@@ -1,30 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import { ViteFaviconsPlugin } from "vite-plugin-favicon2"; // Note: Using vite-plugin-favicon2 (maintained fork)
 
-// https://vitejs.dev/config/
+// https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    ViteFaviconsPlugin({
-      // logo: "./public/logo.png", // Path relative to project root
-      inject: true,
-      favicons: {
-        appName: "EDMOSS",
-        appDescription: "Business Growth Consultants",
-        theme_color: "#1A365D",
-        background: "#FFF",
-        icons: {
-          favicons: true,
-          appleIcon: true,
-          android: true,
-          windows: false,
-          appleStartup: false,
-          coast: false,
-          firefox: false,
-          yandex: false,
-        },
-      },
-    }),
-  ],
+  plugins: [react()],
 });
